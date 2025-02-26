806bffbbf3d1cce33a767177c34b2d330cdc4588
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-26 21:11:08 +0500

    Navbar.jsx File

--- react_course/src/components/Navbar.jsx
+++ react_course/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import logo from '../assets/images/logo.png';
 
-export const Navbar = () => {
+const Navbar = () => {
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -43,3 +43,5 @@ export const Navbar = () => {
     </nav>
   );
 };
+
+export default Navbar;
