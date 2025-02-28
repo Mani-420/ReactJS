bd4b39eb3e4c7d41051ee47f6d9c0eefd700aa23
Mani-420 <tahirabdulrehman46@gmail.com>
2025-02-28 11:58:22 +0500

    Main File

--- react_course/src/App.jsx
+++ react_course/src/App.jsx
@@ -1,7 +1,8 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import HomeCards from './components/HomeCards';
-import JobListing from './components/JobListing';
+import JobListings from './components/JobListings';
+import ViewAllJobs from './components/ViewAllJobs';
 
 const App = () => {
   return (
@@ -9,16 +10,8 @@ const App = () => {
       <Navbar />
       <Hero />
       <HomeCards />
-      <JobListing />
-
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+      <JobListings />
+      <ViewAllJobs />
     </>
   );
 };
